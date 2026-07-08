55ad789f366237b92184a481a0ff01802f026fbe
chenqi <419499748@qq.com>
2026-07-08 13:38:25 +0800

    default init

---
+++ iostemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iostemplate
+//
+//  Created by chenqi on 2026/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iostemplate/iostemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iostemplateApp.swift
+//  iostemplate
+//
+//  Created by chenqi on 2026/7/8.
+//
+
+import SwiftUI
+
+@main
+struct iostemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
